10d03155beab7204b71b6deea3d2e9c34bb42442
Kaique Gabriel <kaiquegabriel973@gmail.com>
2023-07-07 21:38:39 -0300

    TerceiraVersão

--- perfil/views.py
+++ perfil/views.py
@@ -4,13 +4,30 @@ from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
 from .utils import calcula_total
+from extrato.models import Valores
+from django.db.models import Sum
+from datetime import datetime
+from perfil.utils import calcula_equilibrio_financeiro
 
 def home(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    entradas = valores.filter(tipo='E')
+    saidas = valores.filter(tipo='S')
+
+    total_entradas = calcula_total(entradas, 'valor')
+    total_saidas = calcula_total(saidas, 'valor')
+
     contas = Conta.objects.all()
 
     total_contas = calcula_total(contas, 'valor')
 
-    return render(request, 'home.html', {'contas' : contas, 'total_contas' : total_contas})
+    percentual_gastos_essenciais, percentual_gastos_nao_essenciais = calcula_equilibrio_financeiro()
+    return render(request, 'home.html', {'contas' : contas,
+                                        'total_contas' : total_contas,
+                                        'total_entradas': total_entradas,
+                                        'total_saidas': total_saidas,
+                                        'percentual_gastos_essenciais': int(percentual_gastos_essenciais),
+                                        'percentual_gastos_nao_essenciais': int(percentual_gastos_nao_essenciais)})
 
 
 def gerenciar(request):
@@ -83,3 +100,12 @@ def update_categoria(request, id):
     categoria.save()
 
     return redirect('/perfil/gerenciar/')
+
+def dashboard(request):
+    dados = {}
+    categorias = Categoria.objects.all()
+
+    for categoria in categorias:
+        dados[categoria.categoria] = Valores.objects.filter(categoria=categoria).aggregate(Sum('valor'))['valor__sum']
+
+    return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
